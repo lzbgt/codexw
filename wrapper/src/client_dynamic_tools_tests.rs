5fca34780e770aa2bdb66e8ac41a18db2c81e46e
bruce.lu <lzbgt@icloud.com>
2026-03-09 10:53:55 +0800

    Split background shell execution helpers

--- wrapper/src/client_dynamic_tools_tests.rs
+++ wrapper/src/client_dynamic_tools_tests.rs
@@ -1,7 +1,6 @@
 pub(super) use super::dynamic_tool_specs;
 pub(super) use super::execute_dynamic_tool_call;
 pub(super) use super::execute_dynamic_tool_call_with_state;
-pub(super) use crate::background_shells::BackgroundShellManager;
 pub(super) use crate::state::AppState;
 pub(super) use serde_json::json;
 
